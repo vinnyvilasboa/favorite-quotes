6808efaddcf2c8b7aede185f52c8765a9fa3cc0f
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-03-28 21:35:06 -0400

    splitting code

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 const express  = require('express')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
 const methodOverride = require('method-override')
 const app = express()
+const userController = require('./controllers/user')
+const quoteController = require('./controllers/quotes')
 const Quote = require('./models/quote')
 const User = require('./models/user')
 // This is where the old quotes will be stored
@@ -19,27 +22,30 @@ app.engine('jsx', require('express-react-views').createEngine())
 
 
 //MVC SETUP
-app.use(express.static('public'));
-app.use(methodOverride('_method'))
+
 
 //Models
+mongoose.set('strictQuery', true)
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
 
-mongoose.set('strictQuery', true)
 
 //Middleware
+app.use(morgan('tiny'))
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+app.use(express.static('public'));
 
-app.use((req, res, next) => {
-    next()
-})
+// app.use((req, res, next) => {
+//     next()
+// })
 
-app.get('/', (req, res) => {
-    res.render('Home')
-})
+// Routes
+app.use('/', userController)
+app.use('/quotes', quoteController)
+
 
 // email credentials
 const userEmail = process.env.EMAIL;
@@ -125,73 +131,59 @@ async function sendEmails() {
 
 
 
-
 const getAllQuotes = async() => {
-    return await Quote.find({})
+    try{
+        const quotes = await Quote.find({})
+        return quotes
+    } catch(e) {
+        console.log(e)
+        return e
+    }
 }
 
 const getAllUsers = async () => {
-    return await User.find({})
+    try{
+        const users = await User.find({})
+        return users
+    } catch(e) {
+        console.log(e)
+        return e
+    }
 }
 
 //Get all Archives
 const allArchives = async () => {
-    return await Archive.find({})
+    try{
+        const archives = await Archive.find({})
+        return archives
+    } catch(e) {
+        console.log(e)
+        return e
+    }
 }
 
-//Delete User
-app.delete('/user', (req, res) => {
-    User.findOneAndDelete({email: req.body}, (err) => {
-        if(err){
-            res.status(400).send(err)
-        } else {
-            res.render('Home')
-        }
-    })
-})
 
-// Create User
-app.post('/user', (req, res) => {
-    User.create(req.body, (err, createdUser) => {
-        if(err){
-            res.status(403).send(err)
-        } else {
-            res.render('Home')
-        }
-    })
-})
 
-// Create Quote
-app.post('/quote', (req, res) => {
-    Quote.create(req.body, (err, createdQuote) => {
-        if(err){
-            res.status(403).send(err)
-        } else {
-            res.redirect('/')
-        }
-    })
-})
 
 // Create Archive quote
 const newArchive = (quote) => {
-    Archive.create(quote, (err, archivedQuote) => {
-        if(err){
-            console.log(err)
-        } else {
-            console.log('quote archived, ', archivedQuote)
-        }
-    })
+    try{
+        Archive.create(quote)
+    }catch(err){
+        console.log(err)
+    }
 }
 
 //Delete Archive
 const removeArchive = async (id) => {
-   await Archive.findByIdAndDelete(id)
+    try{
+        return await Archive.findByIdAndDelete(id)
+    } catch(e) {
+        console.log(e)
+        return e
+    }
 }
 
-//Catch all
-app.get('/*', (req, res) => {
-    res.render('Home.jsx')
-})
 
 sendEmails().catch(console.error)
 
