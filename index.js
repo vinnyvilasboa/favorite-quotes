11ba862432870935001a4d415c91ce0021458210
Lucas Carlos <lucas2carlos@gmail.com>
2023-03-31 02:12:52 -0400

    fixing bugs

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.engine('jsx', require('express-react-views').createEngine())
 
 //Models
 mongoose.set('strictQuery', true)
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
