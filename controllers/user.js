90e53aa986bc757f737ed69bd2d7afe7d25d1c2a
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-04-02 01:44:57 -0400

    updating routes

--- controllers/user.js
+++ controllers/user.js
@@ -15,35 +15,23 @@ router.get('/', (req, res) =>{
     }
 })
 
+
+// Delete from email
 router.get('/user', (req, res) => {
-    User.findOneAndDelete({email: req.query.email})
+    let deleteEmail = req.query.email
+    User.findOneAndDelete({email: deleteEmail})
     .then(() => {
-        console.log('deleted')
-        res.redirect('https://psych-bite.herokuapp.com')
-        // res.render('Home')
+        console.log('deleted ', deleteEmail)
+        res.render('Unsubscribed', {email: deleteEmail})
     })
     .catch((err) => {
         res.status(400).send(err)
     })
-    console.log(req)
-    console.log(req.body)
-    console.log(req.query.email)
-    res.redirect('Home')
-    // User.findOne({email: req.body})
-    //     .then((user) => {
-    //         console.log(user)
-    //     })
-    //     .catch((err) => {
-    //         console.log('Error ', err)
-    //     })
 })
 
 
 //Delete User
 router.delete('/user', (req, res) => {
-    console.log(req)
-    console.log(req.body)
-    console.log(req.query.email)
     User.findOneAndDelete({email: req.query.email})
         .then(() => {
             console.log('deleted')
