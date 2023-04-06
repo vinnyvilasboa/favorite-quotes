641d2d6865fb24b65aca65b89338188347f35ea3
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-04-05 20:02:26 -0400

    updating code

--- index.js
+++ index.js
@@ -128,8 +128,8 @@ async function sendEmails() {
     ////////////////////////////////
     const rule = new schedule.RecurrenceRule();
     rule.dayOfWeek = [new schedule.Range(0, 7)];
-    rule.hour = 20;
-    rule.minute = 01;
+    rule.hour = 7;
+    rule.minute = 05;
 
     // delay function 
     const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
