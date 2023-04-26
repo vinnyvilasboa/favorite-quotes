24869e5d1d31c0d9312e2b753de96efd3dd0b11c
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-04-26 02:03:13 -0400

    updating index

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -6,9 +6,16 @@ module.exports = {index, createQuote, getAllQuotes}
 
 // Index
 async function index (req, res){
+    const password = req.query.password
+    console.log(password)
     try{
         const quotes = await Quote.find({})
-        res.render('Quotes', {quotes})
+        if(!password){
+            res.render('Access')
+        } else {
+            console.log('Quotes!!!!!!!!!!!!!!!!!')
+            res.render('Quotes', {quotes})
+        } 
     } catch(e) {
         res.status(400).json(e)
     }
