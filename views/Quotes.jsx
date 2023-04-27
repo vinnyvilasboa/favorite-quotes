4faab858cf8097d2f115f8812a36edce53d25c4a
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2023-04-26 22:20:25 -0400

    new Quote button

--- views/Quotes.jsx
+++ views/Quotes.jsx
@@ -8,42 +8,56 @@ class Quotes extends React.Component{
             <Layout>
                 {
                     !access ?
-                        <div className="container">
-                            <form action="/quotes/access" method='POST'>
+                        <form action="/quotes/access" method='POST'>
+                            <div className="form-group row d-flex align-items-center justify-content-center">
+                                <div className="col-md-6 ">
+                                    <label htmlFor="inputPassword" className="sr-only">Enter Password</label>
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        id="inputEmail"
+                                        className="form-control form-control-lg"
+                                        placeholder="Enter password"
+                                        />
+                                </div>
+                                <div className="col-md-3 d-flex align-items-center">
+                                    <input
+                                        type="submit"
+                                        id="submitBtn"
+                                        className="btn btn-primary btn-block btn-lg"
+                                        value="Submit" ></input>
+                                </div>
+                            </div>
+                        </form>
+                    :
+                        <>
+                            <h1>Quotes</h1>
+                            <form action="/quotes/new">
                                 <div className="form-group row d-flex align-items-center justify-content-center">
-                                    <div className="col-md-6 ">
-                                        <label htmlFor="inputPassword" className="sr-only">Enter Password</label>
-                                        <input
-                                            type="password"
-                                            name="password"
-                                            id="inputEmail"
-                                            className="form-control form-control-lg"
-                                            placeholder="Enter password"
-                                            />
-                                    </div>
                                     <div className="col-md-3 d-flex align-items-center">
                                         <input
                                             type="submit"
                                             id="submitBtn"
-                                            className="btn btn-primary btn-block btn-lg"
-                                            value="Submit" ></input>
+                                            className="btn btn-primary btn-block btn-sm"
+                                            value="New Quote" ></input>
                                     </div>
                                 </div>
                             </form>
-                        </div> :
-                        <div>
-                            <h1>Quotes</h1>
                             {
                                 quotes.map((quote) => {
                                     return(
-                                        <div key={quote._id}>
+                                        <div key={quote._id} className="form group d-flex flex-column align-items-center py-4">
                                             <h4>{quote.quote}</h4>
-                                            <p>By {quote.author} </p>
+                                            <p className="my-0">By {quote.author} </p>
+                                            <div className="d-flex col-2 justify-content-around">
+                                                <button>Edit</button>
+                                                <button>Delete</button>
+                                            </div>
                                         </div>
                                     )
                                 })
                             }
-                        </div>
+                        </>
 
                 }
             </Layout>
